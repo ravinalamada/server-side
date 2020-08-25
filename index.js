000437dd025e2c7491ba946cf6b2fcd84041d18f
Prisca Roseline <prisca.ros@onja.org>
2020-08-25 11:22:26 +0300

    installed demon monitor as dependency

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
+const nunjucks = require('nunjucks');
+
 const express = require('express');
 
 const app = express();
 
 app.get('/', (request, response) => {
-  console.log('Request: /');
-  response.sendFile(__dirname + '/index.html');
+  response.render('./views/index.html.njk');
 });
+
 const port = 2000;
+
 app.listen(port, () => {
   console.log(`app is running here: http://localhost:${port}`);
+});
+
+nunjucks.configure({
+  autoescape: true,
+  express: app
 });
